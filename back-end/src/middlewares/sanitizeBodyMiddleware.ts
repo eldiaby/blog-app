4c89f6fc16d6bce2b62ef00d92f7136be9deb247
Eldiaby Hosny <elgdiab@gmail.com>
2025-12-12 13:40:36 +0200

    Add: the sanirize request body middleware

---
+++ back-end/src/middlewares/sanitizeBodyMiddleware.ts
@@ -0,0 +1,14 @@
+import type { NextFunction, Request, Response } from "express";
+
+export const sanitizeBodyMiddleware =
+	(allowedKeys: string[]) =>
+	(req: Request, _: Response, next: NextFunction) => {
+		if (req.body && typeof req.body === "object") {
+			for (const key in req.body) {
+				if (!allowedKeys.includes(key)) {
+					delete req.body[key];
+				}
+			}
+		}
+		next();
+	};
