b81079cc9867d446a1778d4721a28df5ab7e3f3b
Eldiaby Hosny <elgdiab@gmail.com>
2025-12-12 13:44:44 +0200

    Add: the validate request body middleware

---
+++ back-end/src/middlewares/validateBody.ts
@@ -0,0 +1,27 @@
+import type { NextFunction, Request, Response } from "express";
+import { ZodError, type ZodType } from "zod";
+import sendResponse, { ResponseStatus } from "../utils/apiResponse";
+
+export const validateBody = <T>(schema: ZodType<T>) => {
+	return (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const validatedBody: T = schema.parse(req.body);
+			req.body = validatedBody;
+			next();
+		} catch (err) {
+			if (err instanceof ZodError) {
+				return sendResponse(res, {
+					statusCode: 400,
+					status: ResponseStatus.FAIL,
+					message: "Validation failed",
+					data: err.message,
+				});
+			}
+			return sendResponse(res, {
+				statusCode: 500,
+				status: ResponseStatus.ERROR,
+				message: "Server error",
+			});
+		}
+	};
+};
