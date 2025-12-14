4ae06fbce0fc3e17d3498a6bb17e5957ba9bf267
Eldiaby Hosny <elgdiab@gmail.com>
2025-12-14 23:18:48 +0200

    add: cookie-parser middleware

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -4,12 +4,16 @@ import pinoHttp from "pino-http";
 import { v4 as uuidv4 } from "uuid";
 import { logger } from "./config/logger";
 import { requestLogger } from "./middlewares/requestLogger";
+import cookieParser from "cookie-parser";
+
 import { authModule } from "./modules/auth/auth.module";
+import { UserModule } from "./modules/user/user.module";
 
 export const app = express();
 
 app.use(express.json());
 app.use(compression());
+app.use(cookieParser());
 
 /** 1) Correlation ID */
 app.use((req, _, next) => {
@@ -51,5 +55,6 @@ app.use(
 
 /** 4) Routers */
 app.use("/api/v1/auth", authModule.router);
+app.use("/api/v1/users", UserModule.router);
 
 app.get("/", (_, res) => res.send("API is running..."));
